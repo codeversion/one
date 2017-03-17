72de62f20277257c4e083351b508f8475daa275f
cv_li <cv_li@climv.com>
2017-03-17 13:50:40 +0800

    Signed-off-by: cv_li <cv_li@climv.com>

---
+++ class1/class1/Array_max.h
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+class Array_max{
+
+private://声明在类的外部不可访问的隐私成员   
+    int array[10];
+    int max;
+
+
+public://声明在类的外部可以访问的开放的成员函数   
+    void set_value();
+    void max_value();
+    void show_value();
+};
---
+++ class1/class1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  
+#include"Array_max.h"
+using namespace std;
+
+
+int main(){
+    Array_max array1;
+    array1.set_value();//初始化操作  
+    array1.max_value();//求最大数的操作  
+    array1.show_value();//输出最大的数   
+    system("pause");
+    return 0;
+}
