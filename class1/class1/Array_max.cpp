7499b22fc7a3b949169030a493744266b684c453
cv <cv@cv.com>
2017-03-17 14:00:26 +0800

    修改成员函数 Signed-off-by: cv <cv@cv.com>

--- class1/class1/Array_max.cpp
+++ class1/class1/Array_max.cpp
@@ -13,12 +13,14 @@ void Array_max::set_value(){
     }
 };
 void Array_max::max_value(){
+    set_value();
     int i;
     max = array[0];
     for (i = 1; i < 10; i++){
         if (array[i] > max)
             max = array[i];
     }
+    show_value();
 };
 void Array_max::show_value(){
     cout << "max is :" << max << endl;
--- class1/class1/main.cpp
+++ class1/class1/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(){
     Array_max array1;
-    array1.set_value();//初始化操作  
+    //array1.set_value();//初始化操作  
     array1.max_value();//求最大数的操作  
-    array1.show_value();//输出最大的数   
+    //array1.show_value();//输出最大的数   
     system("pause");
     return 0;
 }
